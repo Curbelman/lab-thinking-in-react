f87fb78e1047505db4925697ecde35f19e34d315
Curbelman <111905398+Curbelman@users.noreply.github.com>
2023-01-10 21:47:43 +0100

    displays product row in the table

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -2,15 +2,28 @@ function ProductRow (props) {
     const { products } = props;
 
     return (
-        products.forEach((product) => {
-         <tbody>
-            <tr>
-             <td>{product.name}</td>
-             <td>{product.price}</td>
-            </tr>
-         </tbody>
+        products.map((eachProduct, i) => {
+            return(
+                <tbody>
+                <tr key={i}>
+                    <td>{eachProduct.name}</td>
+                    <td>{eachProduct.price}</td>
+                </tr>
+            </tbody>
+            );
         })
-    );
+    )
+
+    // return (
+    //     products.forEach((product) => {
+    //      <tbody>
+    //         <tr>
+    //          <td>{product.name}</td>
+    //          <td>{product.price}</td>
+    //         </tr>
+    //      </tbody>
+    //     })
+    // );
 }
 
 export default ProductRow;
